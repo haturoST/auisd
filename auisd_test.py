ff92aef3b924b746a28a630499b45bbe2125e90e
unknown <habanerostudio1@gmail.com>
2024-10-25 09:31:36 +0900

    Initial commit

--- auisd_test.py
+++ auisd_test.py
@@ -23,19 +23,19 @@ st.session_state['step'] = 0
 
 #####---> Streamlit Clour(Linux Server)
 # ホームディレクトリを取得してから定義
-# home_dir = os.path.expanduser('~') 
-# save_dir = save_dir = os.path.join(home_dir, 'tmp')
+home_dir = os.path.expanduser('~') 
+save_dir = save_dir = os.path.join(home_dir, 'tmp')
 
-# save_dir = '/tmp'
-# save_dir_materials = save_dir + '/materials'
-# save_dir_temp = save_dir + '/temp'
-# save_dir_outputs = save_dir + '/outputs'
-
-#####---> Windwos Local
-save_dir = 'c:/tmp'
+save_dir = '/tmp'
 save_dir_materials = save_dir + '/materials'
 save_dir_temp = save_dir + '/temp'
 save_dir_outputs = save_dir + '/outputs'
+
+#####---> Windwos Local
+# save_dir = 'c:/tmp'
+# save_dir_materials = save_dir + '/materials'
+# save_dir_temp = save_dir + '/temp'
+# save_dir_outputs = save_dir + '/outputs'
 
 #####---> Mac Local or Linux Local
 # ホームディレクトリを取得してから定義
